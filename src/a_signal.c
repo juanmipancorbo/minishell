b51b660c986a5c03e7fa7b5fd0d3dfe280b17192
pater <agustinpaternoster@gmail.com>
2024-10-08 17:08:47 +0200

    norminete

--- src/a_signal.c
+++ src/a_signal.c
@@ -6,39 +6,39 @@
 /*   By: apaterno <apaterno@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/04 16:27:07 by apaterno          #+#    #+#             */
-/*   Updated: 2024/10/08 16:35:20 by apaterno         ###   ########.fr       */
+/*   Updated: 2024/10/08 17:07:25 by apaterno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-static void sig_handler(int sig)
+static void	sig_handler(int sig)
 {
-	if (sig == SIGINT )
+	if (sig == SIGINT)
 	{
-		write(1,"\n",1);
-		rl_replace_line("",0);
+		write(1, "\n", 1);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 		rl_redisplay();
 	}
 }
 
-static void child_handler(int sig)
+static void	child_handler(int sig)
 {
 	if (sig == SIGINT)
 	{
-		write(1,"\n",1);
-		rl_replace_line("",0);
+		write(1, "\n", 1);
+		rl_replace_line("", 0);
 		rl_on_new_line();
 	}
 	if (sig == SIGQUIT)
 	{
-		write(1,"Quit\n",5);
+		write(1, "Quit\n", 5);
 		rl_on_new_line();
 	}
 }
 
-void init_signals(int i)
+void	init_signals(int i)
 {
 	if (i)
 	{
