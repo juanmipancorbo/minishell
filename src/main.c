72cc370b39dfd8a4af652d29b9852c1a04426212
pater <agustinpaternoster@gmail.com>
2024-10-09 16:51:20 +0200

    inicio

--- src/main.c
+++ src/main.c
@@ -6,11 +6,13 @@
 /*   By: apaterno <apaterno@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/23 20:02:54 by jpancorb          #+#    #+#             */
-/*   Updated: 2024/10/08 16:42:43 by apaterno         ###   ########.fr       */
+/*   Updated: 2024/10/09 16:47:17 by apaterno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+int	g_exit_code;
 
 static void	clean_loop(char *input, t_token *tokens, t_cmd *cmds)
 {
@@ -82,9 +84,10 @@ static void	init_loop(t_utils *utils)
 
 	while (1)
 	{
-		prompt = to_prompt(utils->env_var);
-		input = readline(prompt);
-		free(prompt);
+		// prompt = to_prompt(utils->env_var);
+		// input = readline(prompt);
+		// free(prompt);
+		input = readline("mini>");
 		if (!input)
 		{
 			printf("exit\n");
