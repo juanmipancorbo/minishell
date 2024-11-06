31d2a128623bacf09aa5cf4ce2a503ae26d7a788
Juan Miguel Pancorbo Gutiérrez <jpancorb@car10s2.42barcelona.com>
2024-11-06 18:43:44 +0100

    20% done

--- include/minishell.h
+++ include/minishell.h
@@ -217,7 +217,8 @@ int		replace_env_var(char *var_name, char *new_value, char **env);
 void	add_env_var(char *var_name, char *value, char ***env);
 char	*expand_dollars(const char *value, t_utils *utils);
 int		check_export_name(char *var_name, t_utils *utils);
-
+void	to_no_env(t_utils *utils);
+void	update_shlvl(t_utils *utils);
 
 /* ************************************************************************** */
 /*                                  SIGNALS			                          */
--- src/no_env.c
+++ src/no_env.c
@@ -6,38 +6,54 @@
 /*   By: jpancorb <jpancorb@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 18:56:21 by jpancorb          #+#    #+#             */
-/*   Updated: 2024/11/05 20:58:20 by jpancorb         ###   ########.fr       */
+/*   Updated: 2024/11/06 18:42:27 by jpancorb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
+
+void	update_shlvl(t_utils *utils)
+{
+	int		int_value;
+	char	*ascii_value;
+
+	ascii_value = getenv("SHLVL");
+	if (ascii_value)
+		int_value = ft_atoi(ascii_value) + 1;
+	else
+		int_value = 1;
+	ascii_value = ft_itoa(int_value);
+	if (!ascii_value)
+		return ;
+	if (replace_env_var("SHLVL", ascii_value, utils->env_var))
+		add_env_var("SHLVL", ascii_value, &utils->env_var);
+}
+
 
 void	to_no_env(t_utils *utils)
 {
 	int		i;
 	char	*pwd;
 	char	**env_min;
+	size_t	pwd_len;
 
 	i = -1;
-	env_min = (char **)malloc(sizeof(char *) * 7);
+	env_min = (char **)malloc(sizeof(char *) * 2);
 	if (!env_min)
 		return ;
 	pwd = getcwd(NULL, 0);
 	if (!pwd)
 		pwd = ft_strdup("/home");
-	env_min[0] = malloc(strlen("PWD=") + strlen(pwd) + 1);
-	strcpy(env_min[0], "PWD=");
-	strcat(env_min[0], pwd);
-	env_min[1] = ft_strdup("LS_COLORS=");
-	env_min[2] = ft_strdup("LESSCLOSE=/usr/bin/lesspipe %s %s");
-	env_min[3] = ft_strdup("LESSOPEN=| /usr/bin/lesspipe %s");
-	env_min[4] = ft_strdup("SHLVL=1");
-	env_min[5] = ft_strdup("_=/usr/bin/env");
-	env_min[6] = NULL;
-	while (++i < 6)
+	pwd_len = ft_strlen("PWD=") + ft_strlen(pwd) + 1;
+	env_min[0] = malloc(pwd_len);
+	ft_strcpy(env_min[0], "PWD=");
+	ft_strlcat(env_min[0], pwd, pwd_len);
+	env_min[1] = NULL;
+	while (++i < 1)
 		if (!env_min[i])
 			break ;
 	utils->env_var = env_min;
+	update_shlvl(utils);
 	free(pwd);
 }
 
