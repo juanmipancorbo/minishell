ceed16a8ce28b5e9a649b8ff5b205d18bc8ce363
AgustinPaternoster <agustinpaternoster@gmail.com>
2024-11-24 13:40:14 +0100

    norminette

--- src/a_exit_fn.c
+++ src/a_exit_fn.c
@@ -29,7 +29,7 @@ void	error_msg(char *msg, int exit_code)
 	g_exit_code = exit_code;
 }
 
-void free_hd(int **pipes_fd, t_cmd **cmd, t_utils *utils)
+void	free_hd(int **pipes_fd, t_cmd **cmd, t_utils *utils)
 {
 	free(utils->process_id);
 	free_pipes(pipes_fd, cmd_lst_size(cmd) - 1);
